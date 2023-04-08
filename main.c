0bfa3967ee59f3ba1f311ef440b8727c205aab9f
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-04-08 05:16:34 +0000

    Eliminar, revisar agregados

--- main.c
+++ main.c
@@ -6,23 +6,41 @@ void linkedListNodeDisplay(LinkedListNode *node)
   printf("prioridad: %d\n", node->priority);
 }
 
+void pqInsert(PriorityQueue *pq, PRIORITY_DATA_TYPE priority) {
+  priorityQueueInsert(pq, linkedListNodeCreate(priority));
+}
+
 int main()
 {
-    PriorityQueue pq;
-    priorityQueueInit(&pq);
+  PriorityQueue pq;
+  priorityQueueInit(&pq);
 
-    priorityQueueInsert(&pq, linkedListNodeCreate(14)); 
-    priorityQueueInsert(&pq, linkedListNodeCreate(10));
-    priorityQueueInsert(&pq, linkedListNodeCreate(12));
-    priorityQueueInsert(&pq, linkedListNodeCreate(9));
-    priorityQueueInsert(&pq, linkedListNodeCreate(0));
+  pqInsert(&pq, 14);
+  priorityQueueInsert(&pq, linkedListNodeCreate(10));
+  priorityQueueInsert(&pq, linkedListNodeCreate(12));
+  priorityQueueInsert(&pq, linkedListNodeCreate(9));
+  priorityQueueInsert(&pq, linkedListNodeCreate(0));
 
-    for (int i = 0; i < priorityQueueLenght(&pq); i++)
-    {
-        LinkedListNode *foo = linkedListGet(&pq.data,i);
-        linkedListNodeDisplay(foo);
-    }
-    
+  for (int i = 0; i < priorityQueueLenght(&pq); i++)
+  {
+    LinkedListNode *foo = linkedListGet(&pq.data, i);
+    linkedListNodeDisplay(foo);
+  }
 
-    return 0;
+  printf("Pruebas de eliminacion\n");
+  LinkedListNode *a = priorityQueueRemove(&pq);
+  free(a);
+
+  a = priorityQueueRemove(&pq);
+  free(a);
+
+  for (int i = 0; i < priorityQueueLenght(&pq); i++)
+  {
+    LinkedListNode *foo = linkedListGet(&pq.data, i);
+    linkedListNodeDisplay(foo);
+  }
+
+  priorityQueueFree(&pq);
+
+  return 0;
 }
--- priorityQueue.h
+++ priorityQueue.h
@@ -17,25 +17,70 @@ int priorityQueueLenght(PriorityQueue *queue)
   return linkedListLenght(&(queue->data));
 }
 
-void priorityQueueInit(PriorityQueue *queue) 
+void priorityQueueInit(PriorityQueue *queue)
 {
   linkedListInit(&(queue->data));
 }
 
+/**
+ * libera la memoria
+ * @param queue direccion a la lista 
+ */
+void priorityQueueFree(PriorityQueue *queue) 
+{
+  linkedListFree(&(queue->data));
+}
+
+/**
+ * Revisar el primer valor de la lista
+ * @warning liberar la memoria a la direccion despues de usarla
+ * @param queue direccion a la lista
+ * @return una copia del primer nodo
+ */
+LinkedListNode *priorityQueuePeek(PriorityQueue *queue)
+{
+  LinkedListNode *foo = linkedListNodeCopy(linkedListGet(&(queue->data), 0));
+  return foo;
+}
+
+/**
+ * Eliminar el valor al inicio de la lista
+ * @warning liberar la memoria a la direccion despues de usarla
+ * @param queue direccion a la lista
+ * @return direccion al elemnto eliminado
+ */
+LinkedListNode *priorityQueueRemove(PriorityQueue *queue)
+{
+  LinkedListNode *foo = linkedListNodeCopy(linkedListGet(&(queue->data), 0));
+  linkedListRemove(&(queue->data), 0);
+  return foo;
+}
+
+/**
+ * Insertar el nodo en la lista
+ * @param queue direccion a la lista
+ * @param node direccion al nodo
+ */
 void priorityQueueInsert(PriorityQueue *queue, LinkedListNode *node)
 {
   int i = 0;
 
-  for (i = 0; i < priorityQueueLenght(queue); i++)
+  while (i < priorityQueueLenght(queue))
   {
-    // current node
-    LinkedListNode *current = linkedListGet(&(queue->data),i); 
+    // incrementar i hasta encontrar la posicion con
+    // mayor prioridad que el nodo que queremos insertar
+
+    LinkedListNode *current = linkedListGet(&(queue->data), i);
 
     if (current->priority > node->priority)
-      break; 
+      // lista en orden asendente
+      break;
+
+    i++;
   }
 
-  linkedListInsert(&(queue->data), node, i); 
+  // insertar el nodo en 'i'
+  linkedListInsert(&(queue->data), node, i);
 }
 
 #endif
